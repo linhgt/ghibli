a725c5d6cc84da647f04f95ac0c9af951a1974a3
Linh Nguyen <linh.nguyen2@wsu.edu>
2020-03-19 21:30:20 -0700

    Front page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
+import GhibliStudio from "./img/ghibli-background.jpg";
+import "./css/App.css";
 
 class App extends Component{
     render(){
         return(
-            <div>Welcome to Ghibli Info</div>
+                <div className="container">
+                    <div className="main-content">
+                         <div className="title">
+                             <h2>Welcome to</h2><h1 id="ghibli-info">Ghibli Info!</h1>
+                         </div>
+                    </div>
+                </div>
         )
     }
 }
