12dbd2159cc2f544c7617442c4d2c56fcfe57c3f
Linh Nguyen <linh.nguyen2@wsu.edu>
2020-04-05 00:07:26 -0700

    Use semantic UI for Entry

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,5 +1,5 @@
 import React  from "react";
-import {Image, Button, Icon} from "semantic-ui-react";
+import {Image, Button, Icon, Grid, Header, Label} from "semantic-ui-react";
 import "../css/Entry.css";
 
 const Entry = ({picture, movie, backToList}) => {
@@ -11,12 +11,24 @@ const Entry = ({picture, movie, backToList}) => {
     return(
         <div className="entry-container">
             <div className ="entry-content">
-                <div className="header-title">
-                    <Image id ="picture" src={picture} alt="pic" size="medium"/>
-                    <div id="title">{movie.title}</div>
-                    <div id="year-rating">Year:{movie.release_date}/Rating:{movie.rt_score}/100</div>
-                    <div id="director-producer">Director:{movie.director}/Producer:{movie.producer}</div>
-                </div>
+            <Grid celled >
+                <Grid.Row>
+                    <Grid.Column width={4}>
+                        <Image src={picture}/>
+                    </Grid.Column>
+                    <Grid.Column width={12}>
+                        <Header as="h1">{movie.title}</Header>
+                        <Header as="h3">Year: {movie.release_date}</Header>
+                        <Header as="h3">Director: {movie.director}</Header>
+                        <Header as="h3">Producer: {movie.producer}</Header>
+                        <Header as="h4"></Header>
+                        <Label>
+                            Rating: 
+                            <Label.Detail>{movie.rt_score}/100</Label.Detail>
+                        </Label>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
                 <div className="description">
                     <h1>Synopsis</h1>
                     <p>{movie.description}</p>
